4e7122e54dd5f8b52b809fc6f2910e0c2d7b9510
Adam Thiede <athiede@payitgov.com>
2024-11-01 09:10:55 -0500

    mimic other repo

--- scripts/maint.py
+++ scripts/maint.py
@@ -3,26 +3,21 @@ import os
 import sys
 import argparse
 
-os.system("pwd")
-os.system("ls")
-
-FILE_PATH="stuff/workers.tf"
-MAINT_MODE=True
+FILE_PATH="tf/cloudflare_infra/workers.tf"
 
 parser = argparse.ArgumentParser()
 #-db DATABASE -u USERNAME -p PASSWORD -size 20
 parser.add_argument("-e", "--environment", help="Environment to set maintenance mode. '.<domain>/*' will be appended.")
-parser.add_argument("-m", "--maintenance", action='store_true', help="this argument adds maintenance mode. Run without to disable.")
-parser.add_argument("-d", "--domain", help="Domain, s3licensing.net, s3licensing.com, etc.")
+parser.add_argument("-m", "--maintenance", help="Maintenance, true or false")
 args = parser.parse_args()
 
-def check_if_maint():
+def check_if_maint(mm_site):
     f=open(FILE_PATH, "r")
     lines=f.readlines()
     f.close()
     line_pre="    \""
-    line_post=f".{args.domain}/*\",\n"
-    my_line=line_pre+args.environment+line_post
+    line_post=f"/*\",\n"
+    my_line=line_pre+mm_site+line_post
     if lines.count(my_line)>0:
         print("maintenance is on")
         return True
@@ -30,22 +25,22 @@ def check_if_maint():
         print("maintenance is off")
         return False
 
-def set_maint_mode(on_off):
+def set_maint_mode(on_off, mm_site):
     f=open(FILE_PATH, "r")
     lines=f.readlines()
     f.close()
     f=open(FILE_PATH, "w")
     if on_off:
         patterns_index=lines.index("  patterns = [\n")
-        site_add=f"    \"{args.environment}.{args.domain}/*\",\n"
+        site_add=f"    \"{mm_site}/*\",\n"
         lines.insert(patterns_index+1,site_add)
         newtext=''.join(lines)
         print(newtext)
         f.write(newtext)
     else:
         line_pre="    \""
-        line_post=f".{args.domain}/*\",\n"
-        my_line=line_pre+args.environment+line_post
+        line_post=f"/*\",\n"
+        my_line=line_pre+mm_site+line_post
         lines.remove(my_line)
         newtext=''.join(lines)
         print(newtext)
@@ -53,27 +48,31 @@ def set_maint_mode(on_off):
 
     f.close()
 
+# false/true are strings from github actions, converting/parsing here
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
+
 def __main__():
 
     if args.environment==None:
         print("provide an environment name.")
         sys.exit()
-    if args.domain==None:
-        print("provide a domain name.")
-        sys.exit()
 
-    print(f"env: {args.environment}, maint: {args.maintenance}, path: {FILE_PATH}")
+    mm_site=args.environment
+    maint=str2bool(args.maintenance)
 
-    if args.maintenance:
-        print(f"put {args.environment} into maintenance")
-        in_maint=check_if_maint()
+    print(f"site: {mm_site}, maint: {maint}, path: {FILE_PATH}")
+
+    if maint:
+        print(f"put {mm_site} into maintenance")
+        in_maint=check_if_maint(mm_site)
         if in_maint is False:
-            set_maint_mode(True)
+            set_maint_mode(True, mm_site)
     else:
-        print(f"remove {args.environment} from maintenance")
-        in_maint=check_if_maint()
+        print(f"remove {mm_site} from maintenance")
+        in_maint=check_if_maint(mm_site)
         if in_maint:
-            set_maint_mode(False)
+            set_maint_mode(False, mm_site)
 
 if __name__ == "__main__":
     __main__()
---
+++ tf/cloudflare_infra/workers.tf
@@ -0,0 +1,11 @@
+module "mitest-is_maintenance" {
+  domain      = "s3licensing.net"
+  source      = "../../module/worker"
+  script_path = "maintenance.js"
+  email       = "noreply@payitgov.com"
+  # to add more sites to maintenance mode, just add them here
+  patterns = [
+  ]
+  enabled       = true
+  whitelist_ips = local.worker_ip_whitelist
+}
